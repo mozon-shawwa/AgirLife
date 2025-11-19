41c9ff2abdefb58d07d35315a59105163ec32c3a
unknown <mozon.elshawwa@gmail.com>
2025-11-19 11:03:18 +0200

    Fix CORS and JSON limit

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,12 +3,16 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 module.exports = {
-    global:(app)=>{
-       app.use(cors());
-       app.use(express.json());
-       app.use(express.urlencoded({ extended: true }));
-       app.use(morgan('dev'));
+    global: (app) => {
+        app.use(cors({
+            origin: "*",
+            methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+            allowedHeaders: ["Content-Type", "Authorization"],
+        }));
+        app.use(express.json({ limit: "10mb" }));
+        app.use(express.urlencoded({ extended: true , limit: "10mb"}));
+        app.use(morgan('dev'));
     },
-    auth:require('./auth'),
+    auth: require('./auth'),
     admin: require('./admin')
 }
